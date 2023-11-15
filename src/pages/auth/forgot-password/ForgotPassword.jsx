1321bd51d00025bfd097c744d1fe075f4f33619a
khalid <khankhalid1743@gmail.com>
2023-11-15 14:04:12 +0530

    Feat: Added signin, signup, resetpassword POST with axios

--- src/pages/auth/forgot-password/ForgotPassword.jsx
+++ src/pages/auth/forgot-password/ForgotPassword.jsx
@@ -1,24 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth_forgotPassword } from "../../../services/api/auth/auth.service";
 import "../auth.scss";
 import "./forgotPassword.scss";
 
-const handlesignUp = (e) => {
-  e.preventDefault();
-  console.log("Handle Signup");
-};
-
 const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertType, setAlertType] = useState("");
+  const [responseMessage, setResponseMessage] = useState("");
+
+  const handleForgotPassword = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const response = await auth_forgotPassword({ email });
+      setLoading(false);
+      setEmail("");
+      setShowAlert(false);
+      setAlertType("alert-success");
+      setResponseMessage(response?.data?.message);
+    } catch (error) {
+      setLoading(false);
+      setShowAlert(true);
+      setAlertType("alert-error");
+      setResponseMessage(error?.response?.data?.message);
+    }
+  };
+
   return (
     <div className="form-auth__container sign-up">
-      <form onSubmit={handlesignUp}>
-        {/* <div className="alerts alert-error" role="alert">
-          Error message
-        </div> */}
+      <form onSubmit={handleForgotPassword}>
+        {responseMessage && (
+          <div className={`alerts ${alertType}`} role="alert">
+            {responseMessage}
+          </div>
+        )}
         <h1>Forgot Password</h1>
 
         <span>Use your email to send password reset link</span>
-        <input className="auth_input" type="email" placeholder="Email" />
-        <button className="auth__button">Send Reset Link</button>
+        <input
+          className="auth_input"
+          value={email}
+          style={{ border: `${showAlert ? "1px solid #fa9b8a" : ""}` }}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Email"
+        />
+        <button
+          className={`auth__button ${!email ? "auth__button__disabled" : ""}`}
+          disabled={!email}
+        >
+          {loading ? "Sending Email.." : "Send Reset Link"}
+        </button>
       </form>
     </div>
   );
--- src/pages/auth/signin/SignIn.jsx
+++ src/pages/auth/signin/SignIn.jsx
@@ -3,21 +3,60 @@ import { FaGooglePlusG } from "react-icons/fa6";
 import { FaFacebookF } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
+import { useState, useEffect } from "react";
+import { auth_signIn } from "../../../services/api/auth/auth.service";
 import "./signin.scss";
 
-const handlesignIn = (e) => {
-  e.preventDefault();
-  console.log("Handle Signin");
-};
-
 const SignIn = ({ forgotPassword, tab }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [keepLoggedIn, setKeepLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [alertType, setAlertType] = useState("");
+  const [hasError, setHasError] = useState(false);
+  const [user, setUser] = useState();
+
+  const handlesignIn = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const result = await auth_signIn({ username, password });
+
+      // set logged in to true in local storage
+      // set username in local storage
+      // dispatch user to redux
+      // console.log(result);
+      setUser(result.data.user);
+      setKeepLoggedIn(keepLoggedIn);
+      setHasError(false);
+      setAlertType("alert-success");
+    } catch (error) {
+      setLoading(false);
+      setHasError(true);
+      setAlertType("alert-error");
+      setErrorMessage(error?.response?.data?.message);
+    }
+  };
+
+  useEffect(() => {
+    if (loading && !user) return;
+    if (user) {
+      setLoading(false);
+      console.log("navigate feeds page from login page");
+    }
+  }, [loading, user]);
+
   return (
     <>
       <div className="form-auth__container sign-in">
         <form onSubmit={handlesignIn}>
-          {/* <div className="alerts alert-error" role="alert">
-          Error message
-        </div> */}
+          {hasError && errorMessage && (
+            <div className={`alerts ${alertType}`} role="alert">
+              {errorMessage}
+            </div>
+          )}
           <h1>Sign In</h1>
           <div className="social-icons">
             <a href="/" target="_blank" className="icon">
@@ -34,19 +73,40 @@ const SignIn = ({ forgotPassword, tab }) => {
             </a>
           </div>
           <span>or use your email password</span>
-          <input className="auth_input" type="email" placeholder="Email" />
+          <input
+            value={username}
+            style={{ border: `${hasError ? "1px solid #fa9b8a" : ""}` }}
+            onChange={(e) => setUsername(e.target.value)}
+            className="auth_input"
+            type="text"
+            placeholder="Username"
+          />
           <input
             className="auth_input"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            style={{ border: `${hasError ? "1px solid #fa9b8a" : ""}` }}
             type="password"
             placeholder="Password"
           />
           <div className="keep__me__signedin__label">
             <label className="checkmark-container">
-              <input type="checkbox" />
+              <input
+                type="checkbox"
+                value={keepLoggedIn}
+                onChange={() => setKeepLoggedIn(!keepLoggedIn)}
+              />
               Keep me signed in
             </label>
           </div>
-          <button className="auth__button">Sign In</button>
+          <button
+            className={`auth__button ${
+              !username || !password ? "auth__button__disabled" : ""
+            }`}
+            disabled={!username || !password}
+          >
+            {loading ? "Signing In.." : "Sign In"}
+          </button>
 
           <span
             onClick={() => {
